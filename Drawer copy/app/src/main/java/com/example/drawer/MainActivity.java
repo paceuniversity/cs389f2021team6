131a027acf463f53bb56c0516d49ed858a967538
Linlan Cai <lc255200@gmail.com>
2021-11-28 17:43:33 -0500

    add drawer file

---
+++ Drawer copy/app/src/main/java/com/example/drawer/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.drawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomePageFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_homePage);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_homePage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomePageFragment()).commit();
+                break;
+            case R.id.nav_addItem:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddItemFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_helpUsimprove:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HelpUsImproveFragment()).commit();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this,"share",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this,"Log Out",Toast.LENGTH_SHORT).show();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
